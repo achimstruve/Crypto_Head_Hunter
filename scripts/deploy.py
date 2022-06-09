3cebdf9900d75ebef1a3c5d9a94c54c3a7ca1bed
achimstruve <achim.struve@gmail.com>
2022-06-09 17:26:26 +0200

    add_comment

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,12 @@
 from scripts.helpful_scripts import get_account
 from brownie import CandidateNFT
 
+
 def deploy_candidateNFT():
+    # Get the account
     account = get_account()
     candidateNFT = CandidateNFT.deploy({"from": account})
+
 
 def main():
     deploy_candidateNFT()
