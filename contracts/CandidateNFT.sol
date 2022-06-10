f215bfbe02c9339cab76c93c06b6905b02fb1d61
achimstruve <achim.struve@gmail.com>
2022-06-10 18:26:58 +0200

    compiled

--- contracts/CandidateNFT.sol
+++ contracts/CandidateNFT.sol
@@ -7,8 +7,31 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract CandidateNFT is ERC721 {
     // Token Counter
     uint256 public tokenCounter;
-    
-    constructor () ERC721("Candidate", "CNFT") {
+
+    // Initialize Skills
+    struct Skill {
+        bytes32 name;
+        uint256 level;
+    }
+    Skill public Solidity;
+    Skill public Rust;
+    Skill public Vyper;
+    Skill public Cpp;
+    Skill public Csharp;
+    Skill public Python;
+    Skill public Java;
+    Skill public Golang;
+    Skill public JavaScript;
+    Skill public Front_End_UI;
+    Skill public Art;
+    Skill public Writing;
+    Skill public Data_Science;
+    Skill public Tokenomics;
+    Skill public Marketing;
+    Skill public Leadership;
+    Skill public Moderation;
+
+    constructor() ERC721("Candidate", "CNFT") {
         tokenCounter = 0;
     }
 }
