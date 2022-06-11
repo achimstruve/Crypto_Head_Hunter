cc7762a9e6b6854a95679e2f6f46d3340499703e
achimstruve <achim.struve@gmail.com>
2022-06-11 13:57:40 +0200

    Developed_CandidateNFT

--- contracts/CandidateNFT.sol
+++ contracts/CandidateNFT.sol
@@ -3,35 +3,67 @@
 pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CandidateNFT is ERC721 {
+contract CandidateNFT is ERC721, Ownable {
     // Token Counter
-    uint256 public tokenCounter;
+    uint256 public tokenId;
 
-    // Initialize Skills
+    // Internal information
+    struct Internal_Info {
+        bytes32 name;
+        bytes32 description;
+    }
+    Internal_Info[] internal Internal_Informations;
+
+    // Skills
     struct Skill {
         bytes32 name;
         uint256 experience;
     }
-    Skill public Solidity;
-    Skill public Rust;
-    Skill public Vyper;
-    Skill public Cpp;
-    Skill public Csharp;
-    Skill public Python;
-    Skill public Java;
-    Skill public Golang;
-    Skill public JavaScript;
-    Skill public Front_End_UI;
-    Skill public Art;
-    Skill public Writing;
-    Skill public Data_Science;
-    Skill public Tokenomics;
-    Skill public Marketing;
-    Skill public Leadership;
-    Skill public Moderation;
+    Skill[] public Skills;
+
+    // Mappings
+    mapping(uint256 => Internal_Informations) tokenIdToInternalInfo;
+    mapping(uint256 => Skills) tokenIdToSkills;
+    mapping(uint256 => address) tokenIdToHolder;
 
     constructor() ERC721("Candidate", "CNFT") {
-        tokenCounter = 0;
+        tokenId = 0;
+    }
+
+    function createInternalInfo(bytes32 _name, bytes32 _description)
+        public
+        payable
+        onlyOwner
+    {
+        Internal_Info memory newInfo;
+        newInfo.name = _name;
+        newInfo.description = _description;
+        Internal_Info[].push(newInfo);
+    }
+
+    function getInternalInfo()
+        public
+        payable
+        onlyOwner
+        returns (bytes32[], bytes32[])
+    {
+        return Internal_Info;
+    }
+
+    function createSkill(bytes32 _name, uint256 _experience)
+        public
+        payable
+        onlyOwner
+    {
+        Skill memory newSkill;
+        newSkill.name = _name;
+        newSkill.experience = _experience;
+        Skills.push(newSkill);
+    }
+
+    function getSkills() public payable onlyOwner returns (Skill) {
+        return Skills;
     }
 }
