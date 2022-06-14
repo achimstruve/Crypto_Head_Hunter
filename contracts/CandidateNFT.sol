2ca2e87ffa8390cc7303d464389958b0c2d8c45c
achimstruve <achim.struve@gmail.com>
2022-06-14 09:06:50 +0200

    Added_Price_to_Minting

--- contracts/CandidateNFT.sol
+++ contracts/CandidateNFT.sol
@@ -7,8 +7,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract CandidateNFT is ERC721, Ownable {
-    // Token Counter
+    // Interfaces
+    AggregatorV3Interface public priceFeed;
+    
+    // Parameter
     uint256 public tokenId;
+    uint256 public minUSDprice;
+    address public creatorAddress;
 
     // Internal Information Struct
     struct Internal_Info {
@@ -23,10 +28,31 @@ contract CandidateNFT is ERC721, Ownable {
     }
 
     // Mappings
+    mapping(address => uint256) public addressToAmountSpent;
     mapping(uint256 => Internal_Info[]) internal tokenIdToInternalInfo;
     mapping(uint256 => Skill[]) internal tokenIdToSkills;
 
-    constructor() ERC721("Candidate", "CNDT") {}
+    constructor(address _priceFeed) ERC721("Candidate", "CNDT") {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        minUSDprice = 50; // Set the minimum USD price for one candidate mint to $50
+        creatorAddress = msg.sender;
+    }
+
+    modifier onlyCreator {
+        require(msg.sender == creatorAddress);
+        _;
+    }
+
+    function _setminUSDPrice(uint256 _newPrice) public onlyCreator{
+        require(_newPrice >= 0, "The price has to be equal or more than 0 USD!");
+        minUSDprice = _newPrice;
+    }
+
+    function _changeCreatorAddress(address _newCreator) public onlyCreator{
+        creatorAddress = _newCreator;
+    }
+
+    // Candidate Properties //
 
     function createInternalInfo(
         string memory _name,
@@ -70,6 +96,9 @@ contract CandidateNFT is ERC721, Ownable {
         return skills;
     }
 
+
+    // Minting //
+
     function _mintCandidate(
         address _to,
         uint256 _tokenId,
@@ -78,8 +107,28 @@ contract CandidateNFT is ERC721, Ownable {
         string memory _skillName,
         uint256 _experience
     ) public payable {
+        require(getConversionRate(msg.value)/10**18 >= minUSDprice, "You have to spend more to mint this NFT!");
+        addressToAmountSpent[msg.sender] = msg.value;
         createInternalInfo(_infoName, _description, _tokenId);
         createSkill(_skillName, _experience, _tokenId);
         _safeMint(_to, _tokenId, "");
+    }
+
+    
+    // ETH Price and Conversions //
+
+    function _getPrice() public view returns(uint256){
+        (,int256 price, , , ) = priceFeed.latestRoundData();
+        return uint256(price * 10000000000);
+    }
+
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = _getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
     }
 }
